d2c14fea312d1d04da3f0776b52cd3cd9227aece
roman-g <roman-g@users.noreply.github.com>
2017-08-03 17:58:29 +0500

    Additional helpers

--- SimpleValidation.Priority/PriorityExtensions.cs
+++ SimpleValidation.Priority/PriorityExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using SimpleValidation.Core;
 
 namespace SimpleValidation.Priority
@@ -12,10 +14,41 @@ namespace SimpleValidation.Priority
 		{
 			var orderedRules = rules.GroupBy(x => x.Priority)
 									.OrderBy(x => x.Key)
-									.Select(x => x.Select(y => y.Rule))
-									.Select(x => CombinationHelpers.Union(x.ToArray()))
+									.Select(x => Union(x.ToArray()))
+									.Select(x => x.Rule)
 									.ToArray();
+
 			return CombinationHelpers.Order(orderedRules)(input);
+		}
+
+		public static IRuleWithPriority<TIn, TOut> Combine<TIn, TOut>(IRuleWithPriority<TIn, TOut>[] rules,
+																	  Func<Func<TIn, TOut[]>[], Func<TIn, TOut[]>> combinator)
+		{
+			var priorities = rules.Select(x => x.Priority).Distinct().ToArray();
+			if (priorities.Length > 1)
+				throw new ValidationException($"More than one priority found: [{string.Join(", ", priorities)}]");
+
+			return new RuleWithPriority<TIn, TOut>
+				   {
+					   Priority = priorities.Single(),
+					   Rule = combinator(rules.Select(x => x.Rule).ToArray())
+				   };
+		}
+
+		public static IRuleWithPriority<TIn, TOut> Then<TIn, TOut>(this IRuleWithPriority<TIn, TOut> first,
+																   IRuleWithPriority<TIn, TOut> second)
+		{
+			return Combine(new[] {first, second}, CombinationHelpers.Order);
+		}
+
+		public static IRuleWithPriority<TIn, TOut> Order<TIn, TOut>(params IRuleWithPriority<TIn, TOut>[] rules)
+		{
+			return Combine(rules, CombinationHelpers.Order);
+		}
+
+		public static IRuleWithPriority<TIn, TOut> Union<TIn, TOut>(params IRuleWithPriority<TIn, TOut>[] rules)
+		{
+			return Combine(rules, CombinationHelpers.Union);
 		}
 	}
 }
